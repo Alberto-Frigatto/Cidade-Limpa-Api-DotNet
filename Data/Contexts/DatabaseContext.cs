33ea4e2f30eef2ec22d789cc833dffc43f25e383
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2024-06-21 20:04:10 -0300

    Adicionar arquivos de projeto.

---
+++ Data/Contexts/DatabaseContext.cs
@@ -0,0 +1,28 @@
+﻿using CidadeLimpa.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace CidadeLimpa.Data.Contexts
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<LixeiraModel> Lixeiras { get; set; }
+
+        public DatabaseContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<LixeiraModel>(entity => {
+                entity.ToTable("Lixeira");
+                entity.HasKey(p => p.Id);
+                entity.Property(p => p.Localizacao).IsRequired().HasMaxLength(100);
+                entity.Property(p => p.Capacidade).IsRequired();
+                entity.Property(p => p.Ocupacao).IsRequired().HasPrecision(3, 2);
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+using CidadeLimpa.Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DatabaseConnection");
+builder.Services.AddDbContext<DatabaseContext>(
+    opt => opt.UseOracle(connectionString).EnableSensitiveDataLogging(true)
+);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
