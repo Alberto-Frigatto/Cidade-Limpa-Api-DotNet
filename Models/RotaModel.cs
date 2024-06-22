d2d733cc51ed3407a2d7db9ca58cb4d6a0feb90e
Gustavo Lopes <guhrlopes999@gmail.com>
2024-06-21 23:32:07 -0300

    refactor: Alterar classe RotaModel no namespace Models

--- Models/RotaModel.cs
+++ Models/RotaModel.cs
@@ -3,8 +3,8 @@
     public class RotaModel
     {
         public int Id { get; set; }
-        public string HorarioInicio { get; set; }
-        public string HorarioFim { get; set; }
+        public string? HorarioInicio { get; set; }
+        public string? HorarioFim { get; set; }
         public ICollection<PontoColetaModel> ListaPontosColeta { get; set; }
     }
 }
