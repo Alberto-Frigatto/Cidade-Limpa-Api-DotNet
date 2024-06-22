e458b3bfe6d39f60f65b3340ea933fd4ace1c729
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2024-06-22 17:22:01 -0300

    chore: Mapear ViewModels de lixeira e lixeira para coleta para Models

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
+using AutoMapper;
 using CidadeLimpa.Data.Contexts;
+using CidadeLimpa.Models;
 using CidadeLimpa.Repository;
 using CidadeLimpa.Services;
+using CidadeLimpa.ViewModels.In;
+using CidadeLimpa.ViewModels.Output;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,13 +14,30 @@ builder.Services.AddDbContext<DatabaseContext>(
     opt => opt.UseOracle(connectionString).EnableSensitiveDataLogging(true)
 );
 
-/*adicionando os repositories*/ 
+var mapperConfig = new AutoMapper.MapperConfiguration(c => {
+    c.AllowNullCollections = true;
+    c.AllowNullDestinationValues = true;
+
+    c.CreateMap<CreateLixeiraViewModel, LixeiraModel>();
+    c.CreateMap<UpdateLixeiraViewModel, LixeiraModel>();
+    c.CreateMap<LixeiraModel, DisplayLixeiraViewModel>();
+
+    c.CreateMap<CreateLixeiraParaColetaViewModel, LixeiraParaColetaModel>();
+    c.CreateMap<UpdateLixeiraParaColetaViewModel, LixeiraParaColetaModel>();
+    c.CreateMap<LixeiraParaColetaModel, DisplayLixeiraParaColetaViewModel>();
+});
+
+IMapper mapper = mapperConfig.CreateMapper();
+
+builder.Services.AddSingleton(mapper);
+
+/*adicionando os repositories*/
 builder.Services.AddScoped<ILixeiraRepository, LixeiraRepository>();
 builder.Services.AddScoped<ILixeiraParaColetaRepository, LixeiraParaColetaRepository>();
 
 /*adicionando os services*/
 builder.Services.AddScoped<ILixeiraService, LixeiraService>();
-builder.Services.AddScoped<ILixeiraParaColetaService,  LixeiraParaColetaService>();
+builder.Services.AddScoped<ILixeiraParaColetaService, LixeiraParaColetaService>();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
