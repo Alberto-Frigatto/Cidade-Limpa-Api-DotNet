3db7d96be12dcdf00d62fb738e126f604a6c6357
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2024-06-22 16:22:43 -0300

    chore: Adicionar service e repository para lixeiras  no injetor de dependências

--- Program.cs
+++ Program.cs
@@ -8,6 +8,12 @@ builder.Services.AddDbContext<DatabaseContext>(
     opt => opt.UseOracle(connectionString).EnableSensitiveDataLogging(true)
 );
 
+/*adicionando os repositories*/ 
+builder.Services.AddScoped<ILixeiraRepository, LixeiraRepository>();
+
+/*adicionando os services*/
+builder.Services.AddScoped<ILixeiraService, LixeiraService>();
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
