b19d68d784cbf11208b7ab79047a80479b794fe8
Gustavo Lopes <guhrlopes999@gmail.com>
2024-06-21 23:32:29 -0300

    refactor: Alterar classe CaminhaoModel no namespace Models

--- Models/CaminhaoModel.cs
+++ Models/CaminhaoModel.cs
@@ -3,9 +3,9 @@
     public class CaminhaoModel
     {
         public int Id { get; set; }
-        public string Modelo { get; set; }
+        public string? Modelo { get; set; }
         public int Capacidade { get; set; }
-        public string Placa { get; set; }
+        public string? Placa { get; set; }
         public int IdRota { get; set; }
         public RotaModel Rota { get; set; }
     }
