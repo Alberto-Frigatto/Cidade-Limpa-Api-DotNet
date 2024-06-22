806b7cf95f987600a0b23086c4048027016b0303
Gustavo Lopes <guhrlopes999@gmail.com>
2024-06-22 18:46:50 -0300

    feat: Criar interface IPontoColetaRepository no namespace repository

---
+++ Repository/IPontoColetaRepository.cs
@@ -0,0 +1,10 @@
+﻿using CidadeLimpa.Models;
+
+namespace CidadeLimpa.Repository
+{
+    public interface IPontoColetaRepository
+    {
+        void Add(PontoColetaModel model);
+        void Delete(PontoColetaModel model);
+    }
+}
