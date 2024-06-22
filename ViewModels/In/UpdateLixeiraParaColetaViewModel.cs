cb98b01197ed9eb937c69beae7dfffeeb02ae92f
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2024-06-22 16:57:52 -0300

    feat: Criar UpdateLixeiraParaColetaViewModel no namespace ViewModels.In

---
+++ ViewModels/In/UpdateLixeiraParaColetaViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CidadeLimpa.ViewModels.In
+{
+    public class UpdateLixeiraParaColetaViewModel
+    {
+        [Required(ErrorMessage = "O id da lixeira para coleta é obrigatório")]
+        [Range(1, int.MaxValue, ErrorMessage = "O id da lixeira para coleta é inválido")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O novo status de Ativo é obrigatório")]
+        public bool Ativo { get; set; }
+    }
+}
