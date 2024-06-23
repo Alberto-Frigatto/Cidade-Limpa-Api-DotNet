a5c08998f554b5ebbba697be43f2e7e50eebd1b3
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2024-06-22 21:25:07 -0300

    refactor: Excluir atributo Id de UpdateLixeiraParaColetaViewModel

--- ViewModels/In/UpdateLixeiraParaColetaViewModel.cs
+++ ViewModels/In/UpdateLixeiraParaColetaViewModel.cs
@@ -4,10 +4,6 @@ namespace CidadeLimpa.ViewModels.In
 {
     public class UpdateLixeiraParaColetaViewModel
     {
-        [Required(ErrorMessage = "O id da lixeira para coleta é obrigatório")]
-        [Range(1, int.MaxValue, ErrorMessage = "O id da lixeira para coleta é inválido")]
-        public int Id { get; set; }
-
         [Required(ErrorMessage = "O novo status de Ativo é obrigatório")]
         public bool Ativo { get; set; }
     }
