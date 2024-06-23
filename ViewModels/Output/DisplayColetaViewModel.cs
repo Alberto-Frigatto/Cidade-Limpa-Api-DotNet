9305dc2b08848cb4a9034cb3b384e9d46b36c5a8
Gustavo Lopes <guhrlopes999@gmail.com>
2024-06-23 20:38:54 -0300

    feat: Criar classes display e pagination no namespace viewmodel

---
+++ ViewModels/Output/DisplayColetaViewModel.cs
@@ -0,0 +1,12 @@
+﻿using CidadeLimpa.Models;
+
+namespace CidadeLimpa.ViewModels.Output
+{
+    public class DisplayColetaViewModel
+    {
+        public int Id { get; set; }
+        public CaminhaoModel Caminhao { get; set; }
+        public LixeiraModel Lixeira { get; set; }
+        public string? DataColeta { get; set; }
+    }
+}
---
+++ ViewModels/Output/DisplayRotaViewModel.cs
@@ -0,0 +1,12 @@
+﻿using CidadeLimpa.Models;
+
+namespace CidadeLimpa.ViewModels.Output
+{
+    public class DisplayRotaViewModel
+    {
+        public int Id { get; set; }
+        public string? HorarioInicio { get; set; }
+        public string? HorarioFim { get; set; }
+        public ICollection<PontoColetaModel> ListaPontosColeta { get; set; }
+    }
+}
