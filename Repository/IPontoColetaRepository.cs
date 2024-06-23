d5c62060b40dda2d61733c5151e7411af6bb2f1d
Gustavo Lopes <guhrlopes999@gmail.com>
2024-06-23 17:18:35 -0300

    refactor: Alterar interface IPontoColetaRepository

--- Repository/IPontoColetaRepository.cs
+++ Repository/IPontoColetaRepository.cs
@@ -4,6 +4,7 @@ namespace CidadeLimpa.Repository
 {
     public interface IPontoColetaRepository
     {
+        PontoColetaModel? GetById(int id);
         void Add(PontoColetaModel model);
         void Delete(PontoColetaModel model);
     }
