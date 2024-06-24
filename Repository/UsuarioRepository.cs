e4c97a8f93c79831979bd844cf5c71e22f36fbff
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2024-06-23 22:33:43 -0300

    feat: Criar classe UsuarioRepository no namespace Repository

---
+++ Repository/UsuarioRepository.cs
@@ -0,0 +1,34 @@
+﻿using CidadeLimpa.Data.Contexts;
+using CidadeLimpa.Models;
+
+namespace CidadeLimpa.Repository
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        private readonly DatabaseContext _context;
+
+        public UsuarioRepository(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(UsuarioModel model)
+        {
+            _context.Usuarios.Add(model);
+            _context.SaveChanges();
+        }
+
+        public void Delete(UsuarioModel model)
+        {
+            _context.Usuarios.Remove(model);
+            _context.SaveChanges();
+        }
+        public UsuarioModel? GetByEmail(string email) => _context.Usuarios.FirstOrDefault(e => e.Email == email);
+
+        public void Update(UsuarioModel model)
+        {
+            _context.Usuarios.Update(model);
+            _context.SaveChanges();
+        }
+    }
+}
