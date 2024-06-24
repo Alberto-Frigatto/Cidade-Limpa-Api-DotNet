8071a8663b96e792aaf71876c2d862c21117421c
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2024-06-23 23:31:27 -0300

    feat: Criar classe UsuarioController no namespace Controllers

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using CidadeLimpa.Models;
+using CidadeLimpa.Services;
+using CidadeLimpa.ViewModels.In;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CidadeLimpa.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly IUsuarioService _service;
+        private readonly IMapper _mapper;
+
+        public UsuarioController(IUsuarioService service, IMapper mapper)
+        {
+            _service = service;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public ActionResult Create([FromBody] CreateUsuarioViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var usuario = _mapper.Map<UsuarioModel>(viewModel);
+            _service.CriarUsuario(usuario);
+
+            return NoContent();
+        }
+    }
+}
