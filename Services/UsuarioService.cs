7c5b5ad8c01d13e2d7fe438dd0b42b6ec5c56ace
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2024-06-23 22:40:28 -0300

    feat: Criar interface UsuarioService no namespace Services

---
+++ Services/UsuarioService.cs
@@ -0,0 +1,21 @@
+﻿using CidadeLimpa.Models;
+using CidadeLimpa.Repository;
+
+namespace CidadeLimpa.Services
+{
+    public class UsuarioService : IUsuarioService
+    {
+        private readonly IUsuarioRepository _repository;
+
+        public UsuarioService(IUsuarioRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public void AtualizarUsuario(UsuarioModel model) => _repository.Update(model);
+
+        public void CriarUsuario(UsuarioModel model) => _repository.Add(model);
+
+        public UsuarioModel? ObterUsuarioPorEmail(string email) => _repository.GetByEmail(email);
+    }
+}
