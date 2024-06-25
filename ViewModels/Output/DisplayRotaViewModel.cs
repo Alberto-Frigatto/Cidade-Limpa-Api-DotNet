d028837da5558cdecb4971205211f5201a0bfbce
Gustavo Lopes <guhrlopes999@gmail.com>
2024-06-24 22:01:44 -0300

    refactor: Arrumar atributo ListaPontosColeta na classe DisplayRotaViewModel

--- ViewModels/Output/DisplayRotaViewModel.cs
+++ ViewModels/Output/DisplayRotaViewModel.cs
@@ -7,6 +7,6 @@ namespace CidadeLimpa.ViewModels.Output
         public int Id { get; set; }
         public string? HorarioInicio { get; set; }
         public string? HorarioFim { get; set; }
-        public ICollection<PontoColetaModel> ListaPontosColeta { get; set; }
+        public ICollection<DisplayPontoColetaViewModel> ListaPontosColeta { get; set; }
     }
 }
