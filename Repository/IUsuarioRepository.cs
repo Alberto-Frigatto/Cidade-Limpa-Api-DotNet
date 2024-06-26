6116fd2c8ed7a751418d222b96efd44aa6f30fc5
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2024-06-25 21:51:22 -0300

    refactor: Adicionar método GetByEmailAndSenha em IUsuarioRepository

--- Repository/IUsuarioRepository.cs
+++ Repository/IUsuarioRepository.cs
@@ -4,6 +4,7 @@ namespace CidadeLimpa.Repository
 {
     public interface IUsuarioRepository
     {
+        UsuarioModel? GetByEmailAndSenha(string email, string senha);
         UsuarioModel? GetByEmail(string email);
         void Add(UsuarioModel model);
     }
